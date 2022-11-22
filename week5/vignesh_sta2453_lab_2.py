4fd6dd55521421c25fb2446a221626fbeed60a4f
Vignesh Edithal <vigneshedithal11031997v@gmail.com>
2022-11-22 16:13:56 -0500

    Commit week 5 stuff part 2

--- week5/vignesh_sta2453_lab_2.py
+++ week5/vignesh_sta2453_lab_2.py
@@ -21,6 +21,9 @@ import torch # ML/AI model's and general optimization
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
 
+import scipy
+from tqdm import tqdm
+
 plt.rcParams["figure.figsize"] = (10,6)
 
 """# STA 2453 Lab 2 Submission
@@ -317,6 +320,199 @@ Fit an appropriate finite mixture model to the dataset. Determine the optimal nu
 To achieve a 4 you must appropriately model both the conditional distribution of `HEIGHT ~ HEIGHT.F` and the covariate distribution for `HEIGHT.F` within the context of a finite mixture model. 
 """
 
+# My implementation of FLM
+
+class FiniteLinearModelEM():
+    """Finite linear model using EM algo"""
+    def __init__(self, G: int, data: np.ndarray):
+        """
+        :param G: number of components
+        :type G: int
+        :param data: data to be fitted
+        :type data: np.ndarray
+        """
+
+        if isinstance(data, torch.Tensor):
+            data = data.numpy()
+
+        # define constants
+        self.n = data.shape[0]
+        self.G = G
+
+        # define data
+        self.X = data[:, 1:]
+        self.X = np.concatenate((self.X, np.ones(shape=(self.n, 1))), axis=1)
+        self.y = data[:, 0] 
+
+        # number of covariates
+        self.p = self.X.shape[1]
+
+        # define parameters
+        self.betas = np.random.normal(size=(self.G, self.p))
+        self.sigmas = np.abs(np.random.normal(size=(self.G,)))
+        # Model covariate variance using normal distribution
+        self.covariate_sigmas = np.abs(np.random.normal(size=(self.G,)))
+        # mixing ratios
+        self.w = np.abs(np.random.normal(size=(self.G,)))
+        # w's should sum up to one for all i's and a given j
+        self.w = scipy.special.softmax(self.w)
+        # latent variable matrix of dimension = n * G
+        self.z = np.random.normal(size=(self.n, self.G))
+        # Z_ij should sum up to one for all i's and a given j
+        self.z = scipy.special.softmax(self.z, axis=1)
+
+    def Estep(self) -> np.ndarray:
+        """Compute the expectation step and calculate weights"""
+        dens = np.exp(self.log_density())
+        dens1 = dens * self.w
+        d_sum = dens1.sum(-1)[..., None]
+        self.z = dens1 / d_sum
+
+    def Mstep(self):
+        """
+        Estimate parameters to maximize likelihood
+        """
+        self.w = self.z.sum(axis=0)/self.z.shape[0]
+        for g in range(self.G):
+            self.sigmas[g] = np.sqrt((
+                self.z[:, g] * np.power((self.y - self.X @ self.betas[g, :].T), 2)
+            ).sum() / self.z[:, g].sum())
+        for g in range(self.G):
+            self.covariate_sigmas[g] = np.sqrt((
+                self.z[:, g] * np.power(self.X[:, 0], 2)
+            ).sum() / self.z[:, g].sum())
+        # beta_new = (X' * W * X)^-1 * (X' * W * y)
+        for g in range(self.G):
+            term1 = np.linalg.inv(self.X.T @ np.diag(self.z[:,g]) @ self.X)
+            self.betas[g,:] = term1 @ (self.X.T @ np.diag(self.z[:,g]) @ self.y)
+
+    def fit(self, max_iter=10):
+        """Run EM algorithm"""
+        for it in tqdm(range(max_iter), desc="Running EM algo"):
+            self.Estep()
+            self.Mstep()
+
+            if it % 1 == 0:
+                tqdm.write(f"Loss: {self.objective_function()}")
+
+    def objective_function(self) -> np.ndarray:
+        """
+        Return negative log likelihood objective function to minimize
+
+        :returns: Objective function
+        :rtype: np.ndarray
+        """
+        # n * G 
+        dens = np.exp(self.log_density())
+
+        # What if `dens` has extremely small values?
+        output = - np.log((dens * self.w).sum(-1)).sum()
+
+        return output
+
+    def log_density(self) -> np.ndarray:
+        """
+            Take in covariate X and response y,
+            compute n * G matrix of log densities
+
+            :return: n * G matrix of log densities
+            :rtype: np.ndarray 
+        """
+        ldens = np.zeros(shape=(self.n, self.G))
+
+        # iterate through components
+        for g in range(self.G):
+
+            # compute linear model
+            # element wise multiplication then sum inner axis
+            y_hats = (self.betas[g] * self.X).sum(-1)
+
+            # compute log of gaussian density
+            ldens[:, g] = - 0.5 * np.log([2 * np.pi])
+            ldens[:, g] += - np.log(self.sigmas[g])
+            ldens[:, g] += - 0.5 * np.power(((y_hats - self.y) / self.sigmas[g]), 2)
+
+            # Modelling covariate variance using normal distribution
+            ldens[:, g] += - 0.5 * np.log([2 * np.pi])
+            ldens[:, g] += - np.log(self.sigmas[g])
+            # The second column is just ones, so ignore that
+            ldens[:, g] += - 0.5 * np.power(
+                self.X[:, 0] / self.covariate_sigmas[g], 2
+            )
+        
+        return ldens
+
+    def MAP(self) -> np.ndarray:
+        """Calculate labels using maximum a posteriori"""
+
+        return np.argmax(self.z, axis=1).astype(int)
+
+    def plot_colors_MAP(self):
+        """
+            Plot MAP results
+        """
+
+        predictions = self.MAP()
+        color_palette = sns.color_palette("bright", int(np.max(predictions)) + 1)
+
+        plot_df = np.concatenate((self.y[..., None], self.X), axis=-1)
+        plot_df = pd.DataFrame(plot_df)
+        plot_df = plot_df[[0, 1]]
+
+        plot_df['colors'] = pd.Series(predictions).apply(lambda x: color_palette[x])
+
+        sns.scatterplot(x=plot_df[1], y=plot_df[0], color=plot_df['colors'], s=2.0)
+
+        plt.savefig("practice_flm_fit_colors_map.png")
+        plt.clf()
+
+    def BIC(self) -> float:
+        """Return Bayesian Information criteria"""
+        rho = (
+            self.z.size + self.betas.size + self.sigmas.size
+            + self.w.size + self.covariate_sigmas.size
+        )
+        bic =  -2 * self.objective_function() - rho * np.log(self.y.shape[0])
+        return float(bic)
+
+    def plot(self):
+        """Plot given column"""
+        plot_df = np.concatenate((self.y[..., None], self.X), axis=-1)
+        plot_df = pd.DataFrame(plot_df)
+        plot_df = plot_df[[0, 1]]
+
+        predictions = self.MAP()
+        color_palette = sns.color_palette("bright", int(np.max(predictions)) + 1)
+        plot_df['colors'] = pd.Series(predictions).apply(lambda x: color_palette[x])
+
+        sns.scatterplot(x=plot_df[1], y=plot_df[0], hue=plot_df['colors'], s=2.0)
+
+        for g in range(self.G):
+            y_fits = (self.betas[g] * self.X).sum(-1)
+            plot_df['y_fits'] = y_fits
+            sns.scatterplot(x=plot_df[1], y=plot_df['y_fits'], color='red', s=2.0)
+
+        plt.savefig("practice_flm_fit.png")
+        plt.clf()
+
+    def plot_colors(self, col, labs):
+        """
+            col: reference column
+            labs: integer np.array
+        """
+
+        color_palette = sns.color_palette("bright", int(np.max(labs)) + 1)
+
+        plot_df = np.concatenate((self.y[..., None], self.X), axis=-1)
+        plot_df = pd.DataFrame(plot_df)
+        plot_df = plot_df[[0, col]]
+        plot_df['colors'] = pd.Series(labs).apply(lambda x: color_palette[x])
+
+        sns.scatterplot(x=plot_df[col], y=plot_df[0], hue=plot_df['colors'], s=2.0)
+
+        plt.savefig("practice_flm_fit_colors.png")
+        plt.clf()
+
 # declare X,y 
 
 # set up tensor such that y is in the first column.
@@ -329,6 +525,16 @@ To achieve a 4 you must appropriately model both the conditional distribution of
 
 # do it again with multiple settings of G to figure out which one is the best. 
 # Report the BIC results for multiple G's
+
+flm_data = data_df[["HEIGHT", "HEIGHT.F"]].to_numpy()
+
+flm_data_norm = (flm_data - flm_data.mean())/flm_data.std()
+
+flm = FiniteLinearModelEM(G=2, data=flm_data_norm)
+
+flm.fit(max_iter=30)
+print(f"BIC: {flm.BIC()}")
+flm.plot()
 
 """### Plot Mixture Model Results 
 
@@ -343,7 +549,9 @@ comment on the ability of the finite mixture model to segment the population in 
 
 # get MAP labels. 
 
-# declare colours and plot HEIGHT.F vs HEIGHT.
+# declare colours and plot HEIGHT.F vs HEIGHT. 
+predictions = flm.MAP()
+predictions
 
 """### Classification Table
 Use the following function (`table_counts`) to create a classification table. 
